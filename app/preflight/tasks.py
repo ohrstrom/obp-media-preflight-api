f5ab1f29f2aff56667bee7b52cba72e1823fd043
ohrstrom <ohrstrom@gmail.com>
2017-09-04 10:42:03 +0200

    added logging to task

--- app/preflight/tasks.py
+++ app/preflight/tasks.py
@@ -18,40 +18,9 @@ def preflight_check_task(obj):
 
     log.debug('run preflight check for {}'.format(obj.pk))
 
-    print('**********')
-    print(obj.media_file.path)
-
-
-
-
     r = PreflightRunner(obj.media_file.path)
 
     preflight_result = r.run()
-
-    # r = Renderer()
-    #
-    # obj._skip_content_hash_check = True
-    #
-    # try:
-    #
-    #     mixdown_path = r.render(playlist_id=obj.pk, playlist_uri=obj.remote_uri)
-    #
-    #     with open(mixdown_path, 'rb') as f:
-    #         mixdown_file = File(f)
-    #         obj.status = obj.STATUS_DONE
-    #
-    #         obj.mixdown_file.save('mixdown.mp3', mixdown_file, False)
-    #
-    #     log.info('successfully rendered playlist id: {}'.format(obj.pk))
-    #
-    #
-    # except Exception as e:
-    #
-    #     log.error('error rendering playlist id: {} - {}'.format(obj.pk, e))
-    #     obj.status = obj.STATUS_ERROR
-
-    # cleans all renderer artefacts
-    # r.cleanup()
 
 
     if preflight_result['errors']:
@@ -65,14 +34,18 @@ def preflight_check_task(obj):
 
     # inform remote api
     url = obj.remote_uri
+
+    log.debug('updating remote resource: {}'.format(url))
+
     r = APIClient().patch(
         url,
         json={'result': json.dumps(preflight_result)}
     )
-    print(r.status_code)
-    print(r.text)
 
-    print('check done - delete object')
+    log.debug('API response status code: {}'.format(r.status_code))
+    log.debug('API response text: {}'.format(r.text))
+
+    log.debug('check done - delete instance')
     obj.delete()
 
 
